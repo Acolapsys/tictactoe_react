c6db256684ddc32a7215105ebd939c77f5614a51
Acolapsys <timur_sh@bk.ru>
2020-10-16 15:29:34 +0500

    Updates

---
+++ precache-manifest.a2c6f15552ebc8cd6717fbf3b76695dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de4d93307468a63efa8446beebe7c0e",
+    "url": "/tictactoe_react/index.html"
+  },
+  {
+    "revision": "d4bc98e2c29741755fe8",
+    "url": "/tictactoe_react/static/css/main.cb5251e1.chunk.css"
+  },
+  {
+    "revision": "261d2e9671362c025449",
+    "url": "/tictactoe_react/static/js/2.5c4ea230.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tictactoe_react/static/js/2.5c4ea230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4bc98e2c29741755fe8",
+    "url": "/tictactoe_react/static/js/main.cc59347a.chunk.js"
+  },
+  {
+    "revision": "52ce32f4958354606b42",
+    "url": "/tictactoe_react/static/js/runtime-main.cdda07fb.js"
+  }
+]);
